7345eb73e3c0e1d3ed354de4c1b611d023dc0f5d
SaveliyKarpushin <113942678+SaveliyKarpushin@users.noreply.github.com>
2022-11-07 18:20:45 +0300

    дз-4

---
+++ Выполнение/4.1/Program.cs
@@ -0,0 +1,87 @@
+﻿/*Двумерные массивы
+Для заданной матрицы размером 8x8 найти такие k, при которых k-я строка матрицы совпадает с k-м столбцом.
+Найти сумму элементов в тех строках, которые содержат хотя бы один отрицательный элемент.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 8;                   
+            int[,] mas = new int[N, N];        
+            int[] masi = new int[N];
+            int[] masj = new int[N];
+            int count = 0, ind = 0;
+
+            Random r = new Random();
+
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    mas[i, j] = r.Next(-1, 1);   
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    Console.Write(mas[i, j] + "\t");
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    masi[j] = mas[i, j];
+
+                for (int m = 0; m < N; m++)
+                {
+                    for (int n = 0; n < N; n++)
+                    {
+                        if (masi[n] == mas[n, m])
+                        {
+                            count++;
+                            ind = m;
+                        }
+                        else
+                        {
+                            count = 0;
+                            break;
+                        }
+                    }
+
+                    if (count == N && i == ind)
+                    {
+                        Console.WriteLine("\nk: " + (i + 1));
+                    }
+                }
+            }
+
+            
+            
+            // Сумма элементов в строках с отр. элементом(ми)
+           
+
+            int sum = 0;
+            bool check = false;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    sum += mas[i, j];
+                    if (mas[i, j] < 0) check = true;
+                }
+                if (check)
+                {
+                    Console.WriteLine("Сумма (" + i + "): " + sum);
+                    check = false;
+                }
+                sum = 0;
+            }
+
+        }
+    }
+}
---
+++ Выполнение/ДЗ-4/Program.cs
@@ -0,0 +1,127 @@
+﻿/*Вариант 7 Карпушин УТС-21
+   Одномерные массивы
+В одномерном массиве, состоящем из n-целочисленных элементов, вычислить:
+• номер максимального элемента массива;
+• произведение элементов массива, расположенных между первым и вторым нулевыми элементами.
+Преобразовать массив таким образом, чтобы в первой его половине располагались элементы, стоявшие в нечетных позициях, а во второй половине — элементы, стоявшие в четных позициях.
+ */
+   
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ДЗ_4
+{
+
+    class Function
+    {
+        //функция печати массива на экран
+       
+        public static void PrintArray(int[] a)
+        {
+            foreach (int i in a) Console.Write(i + "  ");
+            Console.WriteLine();
+        }
+       
+
+        //функция поиска максимального элемента в массиве
+        public int IndexOfMaxElement(int[] a, int size)
+        {
+            int max = a[0];
+            int index = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+       
+
+        //функция поиска индексов левого и правого нулей
+        public void ZeroIndex(int[] a, int size, out int leftIndex, out int rightIndex)
+        {
+            leftIndex = rightIndex = 0;
+            //в данном цикле находится индекс самого левого нуля
+            for (int i = 0; i < size; i++)
+            {
+                if (a[i] == 0)
+                {
+                    leftIndex = i;
+                    break;
+                }
+            }
+          
+            //в данном цикле находится индекс самого правого нуля
+            for (int i = size - 1; i >= 0; i--)
+            {
+                if (a[i] == 0)
+                {
+                    rightIndex = i;
+                    break;
+                }
+            }
+           
+        }
+       
+
+        //функция находит произведение элементов между крайними нулями
+        public int Proizvedenie(int[] a, int leftIndex, int rightIndex)
+        {
+            int proiz = 1;
+            for (int i = leftIndex + 1; i < rightIndex; i++) proiz *= a[i];
+            return proiz;
+        }
+       
+
+        //функция перестановки элементов в массиве
+        public void Exchange(int[] a, int size)
+        {
+            int temp; //для обмена значений
+            for (int i = 0, j = 1; i < size / 2; i++, j += 2)
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            int[] array;
+            int n = int.Parse(Console.ReadLine());
+            array = new int[n];
+            Random r = new Random();
+            for(int i = 0; i < n; i++)
+            {
+                array [i] = r.Next(-100, 100); 
+            }
+
+         Function function = new Function();
+            int leftIndex, rightIndex;
+            Console.WriteLine("Исходный массив:" + Environment.NewLine);
+            Function.PrintArray(array);
+            Console.WriteLine();
+
+            Console.Write("Индекс максимального элемента в массиве: " +
+            function.IndexOfMaxElement(array, n) +Environment.NewLine);
+            function.ZeroIndex(array, n, out leftIndex, out rightIndex);
+            Console.WriteLine("Произведение элементов, находящихся между крайними нулями: " +
+            function.Proizvedenie(array, leftIndex, rightIndex) + Environment.NewLine);
+
+            Console.WriteLine("Массив после перестановки элементов: " + Environment.NewLine);
+            function.Exchange(array, n);
+            Function.PrintArray(array);
+
+            Console.ReadKey();
+        }
+    }
+
+}
